fada5c5f0a729dbf7faf5c641107f310fc10053a
Dakota Lillie <dakota.lillie@icloud.com>
2017-12-11 11:29:46 -0500

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,12 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+const EyesOnMe = () => (
+  <button
+    onFocus={() => console.log('Good!')}
+    onBlur={() => console.log('Hey! Eyes on me!')}
+  >
+  </button>
+);
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+import React from 'react';
+
+const Keypad = () => (
+  <input type = "password"
+  onKeyUp = {
+    () => console.log('Entering password...')
+  }
+  />
+
+);
+
+export default Keypad;
